fe8d70d36d5ba0c5022b367e8193760a7259799f
AriSu2904 <arisusanto290401@gmail.com>
2023-11-27 11:38:11 +0700

    Server Declaration

---
+++ index.js
@@ -0,0 +1,12 @@
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+
+});
+
+const {url} = await startStandaloneServer({
+    listen: {port:3000},
+});
+
+console.log(`🚀  Server ready at: ${url}`);
